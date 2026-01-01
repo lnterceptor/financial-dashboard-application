33c56b50188bc15a82293a3c980108aebcb3eed8
Interceptor <mateuszpawel2001@gmail.com>
2026-01-01 18:33:29 +0100

    feat: authentication form validation

--- financial-dashboard-application-frontend/src/app/pages/sign-in/sign-in.ts
+++ financial-dashboard-application-frontend/src/app/pages/sign-in/sign-in.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
@@ -15,20 +15,46 @@ export class SignIn {
   authService = inject(AuthService);
   http = inject(HttpClient);
   router = inject(Router);
+  isValid = true;
+  displayedUsernameError = '';
+  displayedPasswordError = '';
+  minLength = 4;
 
   signInForm = new FormGroup({
-    username: new FormControl('', {nonNullable: true}),
-    password: new FormControl('', {nonNullable: true})
+    username: new FormControl('', [Validators.required, Validators.minLength(this.minLength)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(this.minLength)])
   });
 
   goToRegister(){
     this.router.navigateByUrl('/sign-up');
   }
   signIn(){
-    this.authService.login({username: this.signInForm.controls.username.value, password: this.signInForm.controls.password.value})
+    if(this.signInForm.valid){
+      this.isValid = true;
+    this.authService.login({username: this.signInForm.controls.username.value!, password: this.signInForm.controls.password.value!})
       .subscribe({
         next: (user) => this.router.navigateByUrl('/dashboard')
       });
-    
+    }
+    else{
+      this.isValid = false;
+      this.checkErrors();
+    }
+  }
+  checkErrors(){
+    this.displayedUsernameError = this.containErrors('username');
+    this.displayedPasswordError = this.containErrors('password');
+  }
+  containErrors(input_to_check: string) : string{
+    const contain_errors = this.signInForm.get(input_to_check);
+    if(contain_errors?.errors){
+      if(contain_errors.errors['required']){
+        return 'This field is required';
+      }
+      else if(contain_errors.errors['minlength']){
+        return 'This field must contain at least ' + this.minLength + ' characters.';
+      }
+    }
+    return '';
   }
 }
--- financial-dashboard-application-frontend/src/app/pages/sign-up/sign-up.ts
+++ financial-dashboard-application-frontend/src/app/pages/sign-up/sign-up.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth-service';
 
@@ -14,23 +14,72 @@ export class SignUp {
   authService = inject(AuthService);
   http = inject(HttpClient);
   router = inject(Router);
+  minLengthPassword = 8;
+  minLengthUsername = 6;
+  isValid = false;
+  displayedUsernameError = '';
+  displayedPasswordError = '';
+  displayedEmailError = '';
+  displayedReapeadPasswordError = '';
 
   signUpForm = new FormGroup({
-    username: new FormControl('', {nonNullable: true}),
-    password: new FormControl('',  {nonNullable: true}),
-    email: new FormControl('',  {nonNullable: true}),
-    repeatPassword: new FormControl('',  {nonNullable: true})
+    username: new FormControl('', [Validators.required, Validators.minLength(this.minLengthUsername)]),
+    password: new FormControl('',  [Validators.required, Validators.minLength(this.minLengthPassword)]),
+    email: new FormControl('',  [Validators.required, Validators.email]),
+    repeatPassword: new FormControl('', [Validators.required, Validators.minLength(this.minLengthPassword)])
   });
 
   goToSignIn(){
-    console.log('login');
     this.router.navigateByUrl('/sign-in');
   }
+
   signUp(){
-    const username = this.signUpForm.get('username')?.value;
-    this.authService.register({username: this.signUpForm.controls.username.value, email:this.signUpForm.controls.email.value, password:this.signUpForm.controls.password.value})
+    if(this.signUpForm.valid){
+      if(this.signUpForm.get('password')!.value != this.signUpForm.get('repeatPassword')!.value){
+        this.clearErrors();
+        this.displayedReapeadPasswordError = 'Password and confirmation do not match.';
+        return;
+      }
+      this.isValid = true;
+    this.authService.register({username: this.signUpForm.controls.username.value!, email:this.signUpForm.controls.email.value!, password:this.signUpForm.controls.password.value!})
       .subscribe({next: (user) => 
       this.router.navigateByUrl('/dashboard')
       });
+    }
+    else{
+      this.isValid = false;
+      this.checkErrors();
+    }
+  }
+
+  checkErrors(){
+    this.displayedUsernameError = this.containErrors('username');
+    this.displayedPasswordError = this.containErrors('password');
+    this.displayedEmailError = this.containErrors('email');
+    this.displayedReapeadPasswordError = this.containErrors('repeatPassword');
+  }
+  containErrors(input_to_check: string) : string{
+    const contain_errors = this.signUpForm.get(input_to_check);
+    if(contain_errors?.errors){
+      if(contain_errors.errors['required']){
+        return 'This field is required';
+      }
+      else if(contain_errors.errors['minlength']){
+        if(input_to_check = 'username'){
+          return 'This field must contain at least ' + this.minLengthUsername + ' characters.';
+        }
+        return 'This field must contain at least ' + this.minLengthPassword + ' characters.';
+      }
+      else if(contain_errors.errors['email']){
+        return 'Inputted email is incorrect.';
+      }
+    }
+    return '';
+  }
+  clearErrors(){
+    this.displayedUsernameError = '';
+    this.displayedPasswordError = '';
+    this.displayedEmailError = '';
+    this.displayedReapeadPasswordError = '';
   }
 }
